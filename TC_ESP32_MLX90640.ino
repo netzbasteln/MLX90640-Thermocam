b23e0f97cd68bae6e0ff51aa49c39a77ef29b023
Netzbasteln <44298010+netzbasteln@users.noreply.github.com>
2018-12-16 00:33:40 +0100

    Initial Upload

---
+++ TC_ESP32_MLX90640.ino
@@ -0,0 +1,258 @@
+/**
+ * Based on: 
+ * - https://github.com/wilhelmzeuschner/arduino_thermal_camera_with_sd_and_img_processing.
+ * - https://github.com/sparkfun/SparkFun_MLX90640_Arduino_Example
+ * 
+ * Hardware:
+ * - ESP32: https://www.espressif.com/en/products/hardware/esp32-devkitc/overview
+ * - Sensor: https://shop.pimoroni.com/products/mlx90640-thermal-camera-breakout
+ * - Display: https://www.amazon.de/gp/product/B07DPMV34R/, https://www.pjrc.com/store/display_ili9341.html
+ * 
+ * Needs an ESP32 core fork:
+ * - https://github.com/sparkfun/SparkFun_MLX90640_Arduino_Example/issues/2
+ * - https://github.com/stickbreaker/arduino-esp32
+ */
+
+
+#include <TFT_eSPI.h>
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#include "MLX90640_API.h"
+#include "MLX90640_I2C_Driver.h"
+
+
+#define C_BLUE Display.color565(0,0,255)
+#define C_RED Display.color565(255,0,0)
+#define C_GREEN Display.color565(0,255,0)
+#define C_WHITE Display.color565(255,255,255)
+#define C_BLACK Display.color565(0,0,0)
+#define C_LTGREY Display.color565(200,200,200)
+#define C_DKGREY Display.color565(80,80,80)
+#define C_GREY Display.color565(127,127,127)
+
+
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+
+
+const byte MLX90640_address = 0x33; //Default 7-bit unshifted address of the MLX90640
+#define TA_SHIFT 8 //Default shift for MLX90640 in open air
+paramsMLX90640 mlx90640;
+
+
+TFT_eSPI Display = TFT_eSPI();
+
+
+// Added for measure Temp
+boolean measure = true;
+float centerTemp;
+unsigned long tempTime = millis();
+unsigned long tempTime2 = 0;
+unsigned long batteryTime = 1;
+
+// start with some initial colors
+float minTemp = 20.0;
+float maxTemp = 40.0;
+
+
+// variables for interpolated colors
+byte red, green, blue;
+
+// variables for row/column interpolation
+float intPoint, val, a, b, c, d, ii;
+int x, y, i, j;
+
+
+// array for the 32 x 24 measured pixels
+static float pixels[768];
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hello.");
+
+  // Connect thermal sensor.
+  Wire.begin();
+  Wire.setClock(400000); // Increase I2C clock speed to 400kHz
+  Wire.beginTransmission((uint8_t)MLX90640_address);
+  if (Wire.endTransmission() != 0) {
+    Serial.println("MLX90640 not detected at default I2C address. Please check wiring.");
+  }
+  else {
+    Serial.println("MLX90640 online!");
+  }
+  // Get device parameters - We only have to do this once
+  int status;
+  uint16_t eeMLX90640[832];
+  status = MLX90640_DumpEE(MLX90640_address, eeMLX90640);
+  if (status != 0) Serial.println("Failed to load system parameters");
+  status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
+  if (status != 0) Serial.println("Parameter extraction failed");
+  // Set refresh rate
+  MLX90640_SetRefreshRate(MLX90640_address, 0x05); // Set rate to 8Hz effective - Works at 800kHz
+  // Once EEPROM has been read at 400kHz we can increase
+  Wire.setClock(800000);
+
+  // Set up Display.
+  pinMode(TFT_DC, OUTPUT);
+  SPI.begin();
+  SPI.setFrequency(80000000L);
+  Display.begin();
+  //Display.setRotation(3);
+  Display.fillScreen(C_BLACK);
+
+  // get the cutoff points for the color interpolation routines
+  // note this function called when the temp scale is changed
+  setAbcd();
+  drawLegend();
+}
+
+
+void loop() {
+  tempTime = millis();
+
+  readPixels();
+  setTempScale();
+  drawPicture();
+  drawMeasurement();
+}
+
+
+// Read pixel data from MLX90640.
+void readPixels() {
+  float emissivity = 0.95;
+  
+  for (byte x = 0 ; x < 2 ; x++) //Read both subpages
+  {
+    uint16_t mlx90640Frame[834];
+    int status = MLX90640_GetFrameData(MLX90640_address, mlx90640Frame);
+    if (status < 0)
+    {
+      Serial.print("GetFrame Error: ");
+      Serial.println(status);
+    }
+
+    float vdd = MLX90640_GetVdd(mlx90640Frame, &mlx90640);
+    float Ta = MLX90640_GetTa(mlx90640Frame, &mlx90640);
+
+    float tr = Ta - TA_SHIFT; //Reflected temperature based on the sensor ambient temperature  
+
+    MLX90640_CalculateTo(mlx90640Frame, &mlx90640, emissivity, tr, pixels);
+  }
+}
+
+
+// Show 32x24 sensor data on the top part of the 240x320 screen.
+void drawPicture() {
+  for (y=0; y<24; y++) {
+    for (x=0; x<32; x++) {
+      Display.fillRect(8 + x*7, 8 + y*7, 7, 7, getColor(pixels[(31-x) + (y*32)]));
+    }
+  }
+}
+
+
+// Get color for temp value.
+uint16_t getColor(float val) {
+  /*
+    pass in value and figure out R G B
+    several published ways to do this I basically graphed R G B and developed simple linear equations
+    again a 5-6-5 color display will not need accurate temp to R G B color calculation
+
+    equations based on
+    http://web-tech.ga-usa.com/2012/05/creating-a-custom-hot-to-cold-temperature-color-gradient-for-use-with-rrdtool/index.html
+    
+  */
+
+  red = constrain(255.0 / (c - b) * val - ((b * 255.0) / (c - b)), 0, 255);
+
+  if ((val > minTemp) & (val < a)) {
+    green = constrain(255.0 / (a - minTemp) * val - (255.0 * minTemp) / (a - minTemp), 0, 255);
+  }
+  else if ((val >= a) & (val <= c)) {
+    green = 255;
+  }
+  else if (val > c) {
+    green = constrain(255.0 / (c - d) * val - (d * 255.0) / (c - d), 0, 255);
+  }
+  else if ((val > d) | (val < a)) {
+    green = 0;
+  }
+
+  if (val <= b) {
+    blue = constrain(255.0 / (a - b) * val - (255.0 * b) / (a - b), 0, 255);
+  }
+  else if ((val > b) & (val <= d)) {
+    blue = 0;
+  }
+  else if (val > d) {
+    blue = constrain(240.0 / (maxTemp - d) * val - (d * 240.0) / (maxTemp - d), 0, 240);
+  }
+
+  // use the displays color mapping function to get 5-6-5 color palet (R=5 bits, G=6 bits, B-5 bits)
+  return Display.color565(red, green, blue);
+}
+
+
+void setTempScale() {
+  minTemp = 255;
+  maxTemp = 0;
+
+  for (i = 0; i < 768; i++) {
+    minTemp = min(minTemp, pixels[i]);
+    maxTemp = max(maxTemp, pixels[i]);
+  }
+
+  setAbcd();
+  drawLegend();
+}
+
+
+// Function to get the cutoff points in the temp vs RGB graph.
+void setAbcd() {
+  a = minTemp + (maxTemp - minTemp) * 0.2121;
+  b = minTemp + (maxTemp - minTemp) * 0.3182;
+  c = minTemp + (maxTemp - minTemp) * 0.4242;
+  d = minTemp + (maxTemp - minTemp) * 0.8182;
+}
+
+
+// Draw a legend.
+void drawLegend() {
+  float inc = (maxTemp - minTemp) / 224.0;
+  j = 0;
+  for (ii = minTemp; ii < maxTemp; ii += inc) {
+    Display.drawFastVLine(8+ + j++, 292, 20, getColor(ii));
+  }
+
+  Display.setTextFont(2);
+  Display.setTextSize(1);
+  Display.setCursor(8, 272);
+  Display.setTextColor(TFT_WHITE, TFT_BLACK);
+  Display.print(String(minTemp).substring(0, 5));
+
+  Display.setCursor(192, 272);
+  Display.setTextColor(TFT_WHITE, TFT_BLACK);
+  Display.print(String(maxTemp).substring(0, 5));
+
+  Display.setTextFont(NULL);
+}
+
+
+// Draw a circle + measured value.
+void drawMeasurement() {
+
+  // Mark center measurement
+  Display.drawCircle(120, 8+84, 3, TFT_WHITE);
+
+  // Measure and print center temperature
+  centerTemp = (pixels[383 - 16] + pixels[383 - 15] + pixels[384 + 15] + pixels[384 + 16]) / 4;
+  Display.setCursor(86, 214);
+  Display.setTextColor(TFT_WHITE, TFT_BLACK);
+  Display.setTextFont(2);
+  Display.setTextSize(2);
+  Display.print(String(centerTemp).substring(0, 5) + " °C");
+}
